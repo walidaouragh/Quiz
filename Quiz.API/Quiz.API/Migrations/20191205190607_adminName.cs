d6406dc0f2313296551fa7d8b725e0f572ac06f5
waouragh <waouragh@activeaero.com>
2019-12-06 10:34:10 -0500

    Style home buttons

---
+++ Quiz.API/Quiz.API/Migrations/20191205190607_adminName.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Quiz.API.Migrations
+{
+    public partial class adminName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
