24f649600963d6770b26b1428d6e29a430f6c4d7
waouragh <waouragh@activeaero.com>
2020-01-27 16:07:03 -0500

    Added employee dashboard

--- Quiz-Web/src/app/quiz/employee-dashboard/employee-dashboard.component.ts
+++ Quiz-Web/src/app/quiz/employee-dashboard/employee-dashboard.component.ts
@@ -1,15 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { QuizService } from '../../_services/quiz.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { IEmployeeAdmin } from '../../_types/IEmployee';
+import ITester = namespace.ITester;
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
-  selector: 'app-employee-dashboard',
-  templateUrl: './employee-dashboard.component.html',
-  styleUrls: ['./employee-dashboard.component.scss']
+	selector: 'app-employee-dashboard',
+	templateUrl: './employee-dashboard.component.html',
+	styleUrls: ['./employee-dashboard.component.scss'],
+	encapsulation: ViewEncapsulation.None
 })
 export class EmployeeDashboardComponent implements OnInit {
+	constructor(private quizService: QuizService, private router: Router, private route: ActivatedRoute) {}
 
-  constructor() { }
+	public testers: ITester[];
+	public employees: IEmployeeAdmin[];
+	public isLoadingResults: boolean;
+	public schoolId: number;
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+		this.schoolId = +this.route.snapshot.paramMap.get('schoolId');
+		this.getEmployees();
+		this.getTesters();
+	}
 
+	public getTesters(): void {
+		this.isLoadingResults = true;
+
+		this.quizService.getTesters(this.schoolId).subscribe(
+			(users: ITester[]) => {
+				this.testers = users;
+				this.isLoadingResults = false;
+			},
+			(error: HttpErrorResponse) => {
+				this.isLoadingResults = false;
+				console.log(error);
+			}
+		);
+	}
+
+	public getEmployees(): void {
+		this.isLoadingResults = true;
+
+		this.quizService.getEmployees(this.schoolId).subscribe(
+			(employees: IEmployeeAdmin[]) => {
+				this.employees = employees;
+				this.isLoadingResults = false;
+			},
+			(error: HttpErrorResponse) => {
+				this.isLoadingResults = false;
+				console.log(error);
+			}
+		);
+	}
 }
